9bff6777035c3398d82fc2b36e5fdf393d5bc0c3
EREH <150424356+sihamErmk@users.noreply.github.com>
2025-06-18 10:38:48 +0100

    variables

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	//variables
+	var name string = "Eresih"
+	fmt.Println("My name is", name)
+
+	var test = "this is  a test"
+	var test2 string
+	fmt.Println(test, test2)
+
+	// another  way  to declare variables
+	name2 := "Ereh"
+	fmt.Println(name2)
+}
